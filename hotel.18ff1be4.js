d667fa2df23d84dd13c6c55e352e70b69482814c
ElizPery <113451264+ElizPery@users.noreply.github.com>
2022-12-05 08:58:43 +0000

    Deploying to gh-pages from @ ElizPery/project-mimino@7c0a49dc971fbe866e855715fcc43b3f96b4dab9 🚀

--- hotel.18ff1be4.js
+++ hotel.18ff1be4.js
@@ -1,2 +1,2 @@
 new Swiper(".swiper",{loop:!0,slidesPerView:"auto",spaceBetween:20,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},breakpoints:{1024:{slidesPerView:2,spaceBetween:40}}});
-//# sourceMappingURL=index.3623fbe5.js.map
+//# sourceMappingURL=hotel.18ff1be4.js.map
